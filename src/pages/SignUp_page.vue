ccf9130f57f279b7e4439fe26bab720fc2ab7116
Ali Birashk <alibirashk82@gmail.com>
2024-05-16 22:51:55 +0330

    signup page procces_1

---
+++ src/pages/SignUp_page.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-white text-black">
+      <q-toolbar>
+        <q-toolbar-title class="text-weight-bold">
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          Memos
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+<script>
+export default {
+  name: 'SignUp',
+}
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,18 +1,21 @@
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/signup",
+    component: () => import("pages/SignUp_page.vue"),
+  },
 
-export default routes
+  {
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
+
+export default routes;
