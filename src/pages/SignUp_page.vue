b704999208b44da89307f4c0cd4be7c19d3e2a71
Ali Birashk <alibirashk82@gmail.com>
2024-05-17 20:39:30 +0330

    signup page procces_2

--- src/pages/SignUp_page.vue
+++ src/pages/SignUp_page.vue
@@ -4,15 +4,36 @@
     <q-header elevated class="bg-white text-black">
       <q-toolbar>
         <q-toolbar-title class="text-weight-bold">
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-          Memos
+          <q-icon name="fa-solid fa-dove" size="lg" color="primary" class="q-pa-md, absolute-center"></q-icon>
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
     <q-page-container>
+      <q-page class="flex flex-center bg-grey-2">
+        <q-card class="q-pa-md shadow-2 my_card" bordered>
+          <q-card-section class="text-center">
+            <div class="dark text-h5 text-weight-bold">Sign up</div>
+            <div class="text-grey-8">Sign up below to access the website</div>
+          </q-card-section>
+          <q-card-section>
+            <q-input dense outlined v-model="name" label="Your name" class="q-pa-sm"></q-input>
+            <q-input dense outlined v-model="email" label="Email Address" class="q-pa-sm"></q-input>
+            <q-input dense outlined v-model="date" type="date" class="q-pa-sm"></q-input>
+          </q-card-section>
+          <q-card-section>
+            <q-btn style="border-radius: 8px;" color="primary" rounded size="md" label="Sign in" no-caps
+              class="full-width"></q-btn>
+          </q-card-section>
+          <q-card-section class="text-center q-pt-none">
+            <div class="text-grey-8">Already have an account?
+              <a href="#" class="text-dark text-weight-bold" style="text-decoration: none">Sign
+                in.</a>
+            </div>
+          </q-card-section>
+
+        </q-card>
+      </q-page>
       <router-view />
     </q-page-container>
 
