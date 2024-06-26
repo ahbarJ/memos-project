f194e0beb99edbceb4e31680d1de123b8183a93c
Akbar <ahbarurmia@gmail.com>
2024-06-26 15:21:08 +0330

    memo list component

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -32,6 +32,58 @@
       </div>
     </div>
     <q-separator size="10px" color="grey-2" class="custom-separator" />
+    <q-list separator>
+      <q-item class="q-py-md">
+        <q-item-section avatar top>
+          <q-avatar>
+            <q-icon name="fa-solid fa-user" size="lg" />
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-subtitle1"
+            ><strong>Akbar Jalili</strong
+            ><span class="text-grey-7 q-mx-sm"> @ahbar.j </span></q-item-label
+          >
+          <q-item-label class="memo-cap text-body1">
+            This is my first memo in the Memos app.
+          </q-item-label>
+          <div class="row q-mt-md justify-between">
+            <q-btn color="grey" icon="far fa-comment" size="sm" flat round />
+            <q-btn color="grey" icon="fas fa-retweet" size="sm" flat round />
+            <q-btn color="grey" icon="far fa-heart" size="sm" flat round />
+            <q-btn color="grey" icon="fas fa-trash" size="sm" flat round />
+          </div>
+        </q-item-section>
+
+        <q-item-section side top> 1 min ago </q-item-section>
+      </q-item>
+      <q-item class="q-py-md">
+        <q-item-section avatar top>
+          <q-avatar>
+            <q-icon name="fa-solid fa-user" size="lg" />
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-subtitle1"
+            ><strong>Akbar Jalili</strong
+            ><span class="text-grey-7 q-mx-sm"> @ahbar.j </span></q-item-label
+          >
+          <q-item-label class="memo-cap text-body1">
+            This is my second memo in the Memos app.
+          </q-item-label>
+          <div class="row q-mt-md justify-between">
+            <q-btn color="grey" icon="far fa-comment" size="sm" flat round />
+            <q-btn color="grey" icon="fas fa-retweet" size="sm" flat round />
+            <q-btn color="grey" icon="far fa-heart" size="sm" flat round />
+            <q-btn color="grey" icon="fas fa-trash" size="sm" flat round />
+          </div>
+        </q-item-section>
+
+        <q-item-section side top> 5 min ago </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
@@ -49,9 +101,11 @@ export default {
 <style lang="sass">
 .memoInput
   textarea
-    font-size: 19px
+    font-size: 20px
     line-height: 1.3 !important
 .custom-separator
   border-top: 1px $grey-4 solid
   border-bottom: 1px $grey-4 solid
+.memo-cap
+  white-space: pre-line
 </style>
